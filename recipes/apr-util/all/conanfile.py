c31844111399ac3fd38598f9dc7b58e97f478213
Uilian Ries <uilianr@jfrog.com>
2026-04-30 15:04:53 +0200

    Remove unused import
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

--- recipes/apr-util/all/conanfile.py
+++ recipes/apr-util/all/conanfile.py
@@ -7,7 +7,6 @@ from conan.tools.env import VirtualRunEnv
 from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rm, rmdir
 from conan.tools.gnu import Autotools, AutotoolsDeps, AutotoolsToolchain
 from conan.tools.layout import basic_layout
-from conan.tools.scm import Version
 import os
 
 required_conan_version = ">=2.1"
